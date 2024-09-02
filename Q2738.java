c209ee9befc09356911f3d6011fd9299de1f5d87
Kim JaeMin <2098kjm@naver.com>
2024-09-02 18:06:51 +0900

    Baekjoon Q2738

---
+++ Q2738.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+    Q.
+    N*M크기의 두 행렬 A와 B가 주어졌을 때, 두 행렬을 더하는 프로그램을 작성하시오.
+
+    Input.
+    첫째 줄에 행렬의 크기 N 과 M이 주어진다.
+    둘째 줄부터 N개의 줄에 행렬 A의 원소 M개가 차례대로 주어진다.
+    이어서 N개의 줄에 행렬 B의 원소 M개가 차례대로 주어진다. N과 M은 100보다 작거나 같고,
+    행렬의 원소는 절댓값이 100보다 작거나 같은 정수이다.
+
+    Output.
+    첫째 줄부터 N개의 줄에 행렬 A와 B를 더한 행렬을 출력한다. 행렬의 각 원소는 공백으로 구분한다.
+
+ */
+
+public class Q2738 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String matrix = br.readLine();
+        int N = Integer.parseInt(matrix.split(" ")[0]);
+        int M = Integer.parseInt(matrix.split(" ")[1]);
+        int[][] mat = new int[N][M];
+        int[][] rix = new int[N][M];
+
+        int i;
+        int i;
+        for(i = 0; i < N; ++i) {
+            matrix = br.readLine();
+
+            for(i = 0; i < M; ++i) {
+                mat[i][i] = Integer.parseInt(matrix.split(" ")[i]);
+            }
+        }
+
+        for(i = 0; i < N; ++i) {
+            matrix = br.readLine();
+
+            for(i = 0; i < M; ++i) {
+                rix[i][i] = Integer.parseInt(matrix.split(" ")[i]);
+            }
+        }
+
+        int[][] result = new int[N][M];
+
+        int j;
+        for(i = 0; i < N; ++i) {
+            for(j = 0; j < M; ++j) {
+                result[i][j] = mat[i][j] + rix[i][j];
+            }
+        }
+
+        for(i = 0; i < N; ++i) {
+            for(j = 0; j < M; ++j) {
+                System.out.print(result[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
